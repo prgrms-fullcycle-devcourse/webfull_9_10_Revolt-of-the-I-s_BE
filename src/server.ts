0c713046b58265ef9a50eff3954b1dc9d48cb4d5
gunyoung5 <jimhappy5745@gmail.com>
2026-03-24 16:38:45 +0900

    auth

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,36 @@
-import dotenv from "dotenv";
-import app from "./app";
-import "./config/db";
+// 서버 설정과 외부 서비스 초기화 담당
+import path from "path";
+import * as dotenv from "dotenv";
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+import app from "./app"
+import "./config/db"
+// import "./config/pusher"
+import { Request, Response, NextFunction } from 'express';
 
-dotenv.config();
+const PORT = process.env.PORT
 
-require("./config/pusher");
+interface CustomError extends Error {
+  status?: number;
+}
 
-const PORT = process.env.PORT || 3000;
+// 서버 에러 모듈
+app.use((err: CustomError, req: Request, res: Response, next: NextFunction) => {
+    console.error('--- ERROR 발생 ---');
+    console.error(err.stack); 
+
+    const statusCode = err.status || 500;
+    const message = err.message || "서버 내부 에러가 발생했습니다.";
+
+    res.status(statusCode).json({
+        success: false,
+        message: message
+    });
+});
 
 app.listen(PORT, () => {
   console.log(`
   ################################################
   🛡️  Server listening on port: ${PORT}
   ################################################
-  `);
-});
+  `)
+})
