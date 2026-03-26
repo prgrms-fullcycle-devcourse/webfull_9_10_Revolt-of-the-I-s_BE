881f11e941c6f4e342576dee8fff1573ab4fb1d0
gunyoung5 <jimhappy5745@gmail.com>
2026-03-26 10:50:01 +0900

    feat: validata

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,3 +1,5 @@
+import { Request, Response, NextFunction } from "express";
+
 // ID 유효성 검사
 // teamId, taskId, commentId
 export const isValidId = (id: string | string[] | undefined): boolean => {
@@ -8,4 +10,16 @@ export const isValidId = (id: string | string[] | undefined): boolean => {
 // title, content
 export const isValidString = (value: any): boolean => {
   return !!value && typeof value === "string" && value.trim() !== "";
+};
+
+// 회원가입 유효성 검사
+export const validate = (schema: any) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await schema.parseAsync(req.body);
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
 };
